7e73e0b0bcd8212263545a99ce3495b7ed0aada5
Aman <aman4594@gmail.com>
2020-05-19 07:04:18 +0800

    Added Lockdown Correlation Files

---
+++ wuhan_data/Processed/correlation_lockdown.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import numpy as np
+import math
+
+wuhan_data = pd.read_csv("wuhan_covid_aqi.csv")
+
+wuhan_data['Date'] = pd.to_datetime(wuhan_data['Date'], dayfirst = False, yearfirst = False)
+wuhan_data.sort_values(by=['Date'], inplace=True, ascending=True)
+wuhan_data = wuhan_data.loc[(wuhan_data['Date'] > '2020-01-13') & (wuhan_data['Date'] <= '2020-03-14')]
+print(wuhan_data)
+
+
+
+mean_pm25 = wuhan_data['pm25'].mean()
+mean_pm10 = wuhan_data['pm10'].mean()
+mean_o3 = wuhan_data['o3'].mean()
+mean_no2 = wuhan_data['no2'].mean()
+mean_so2 = wuhan_data['so2'].mean()
+
+mean_corona = wuhan_data['Daily'].mean()
+
+xix_pm25 = []
+xix_pm10 = []
+xix_o3 = []
+xix_no2 = []
+xix_so2 = []
+yiy_corona = []
+
+for i in range(0,len(wuhan_data)):
+    xix_pm25.append(wuhan_data['pm25'].iloc[i] - mean_pm25)
+    xix_pm10.append(wuhan_data['pm10'].iloc[i] - mean_pm10) 
+    xix_o3.append(wuhan_data['o3'].iloc[i] - mean_o3)
+    xix_no2.append(wuhan_data['no2'].iloc[i] - mean_no2)
+    xix_so2.append(wuhan_data['so2'].iloc[i] - mean_so2)
+    yiy_corona.append(wuhan_data['Daily'].iloc[i] - mean_corona)
+
+s_pm25 = 0
+s_pm10 = 0
+s_o3 = 0
+s_no2 = 0
+s_so2 = 0
+s_corona = 0
+
+for j in range(0,len(xix_pm25)):
+    s_pm25 = s_pm25 +  (xix_pm25[j] * xix_pm25[j])
+    s_pm10 = s_pm10 +  (xix_pm10[j] * xix_pm10[j])
+    s_o3 = s_o3 +  (xix_o3[j] * xix_o3[j])
+    s_no2 = s_no2 +  (xix_no2[j] * xix_no2[j])
+    s_so2 = s_so2 +  (xix_so2[j] * xix_so2[j])
+    s_corona = s_corona +  (yiy_corona[j] * yiy_corona[j])
+
+s_pm25 = math.sqrt(s_pm25/(len(wuhan_data) - 1)) 
+s_pm10 = math.sqrt(s_pm10/(len(wuhan_data) - 1)) 
+s_o3 = math.sqrt(s_o3/(len(wuhan_data) - 1)) 
+s_no2 = math.sqrt(s_no2/(len(wuhan_data) - 1)) 
+s_so2 = math.sqrt(s_so2/(len(wuhan_data) - 1)) 
+s_corona = math.sqrt(s_corona/(len(wuhan_data) - 1)) 
+
+r_pm25 = 0
+r_pm10 = 0
+r_o3 = 0
+r_no2 = 0
+r_so2 = 0
+
+for k in range(0,len(xix_pm25)):
+    r_pm25 = r_pm25 + ((xix_pm25[k] * yiy_corona[k]) / (s_pm25 * s_corona))
+    r_pm10 = r_pm10 + ((xix_pm10[k] * yiy_corona[k]) / (s_pm10 * s_corona))
+    r_o3 = r_o3 + ((xix_o3[k] * yiy_corona[k]) / (s_o3 * s_corona))
+    r_no2 = r_no2 + ((xix_no2[k] * yiy_corona[k]) / (s_no2 * s_corona))
+    r_so2 = r_so2 + ((xix_so2[k] * yiy_corona[k]) / (s_so2 * s_corona))
+
+r_pm25 = r_pm25/(len(wuhan_data) - 1)
+r_pm10 = r_pm10/(len(wuhan_data) - 1)
+r_o3 = r_o3/(len(wuhan_data) - 1)
+r_no2 = r_no2/(len(wuhan_data) - 1)
+r_so2 = r_so2/(len(wuhan_data) - 1)
+
+print('Using Basic Statistics:')
+print('Correlation Coefficient (PM2.5 and Daily Corona Cases): ', r_pm25)
+print('Correlation Coefficient (PM10 and Daily Corona Cases): ', r_pm10)
+print('Correlation Coefficient (O3 and Daily Corona Cases): ', r_o3)
+print('Correlation Coefficient (NO2 and Daily Corona Cases): ', r_no2)
+print('Correlation Coefficient (SO2 and Daily Corona Cases): ', r_so2)
+
+print()
+print('Using Pearson Method: ')
+print('Correlation Coefficient (PM2.5 and Daily Corona Cases): \n', wuhan_data[['pm25','Daily']].corr(method='pearson'))
+print('Correlation Coefficient (PM10 and Daily Corona Cases): \n', wuhan_data[['pm10','Daily']].corr(method='pearson'))
+print('Correlation Coefficient (O3 and Daily Corona Cases): \n', wuhan_data[['o3','Daily']].corr(method='pearson'))
+print('Correlation Coefficient (NO2 and Daily Corona Cases): \n', wuhan_data[['no2','Daily']].corr(method='pearson'))
+print('Correlation Coefficient (SO2 and Daily Corona Cases): \n', wuhan_data[['so2','Daily']].corr(method='pearson'))
